72476dbce45dfe0450dd31658c83f4aba79bc3ff
Matheus <matheus.mleall@gmail.com>
2024-03-19 08:53:40 -0300

    created new ep to get an event without being an admin

--- src/routes/site.ts
+++ src/routes/site.ts
@@ -1,9 +1,12 @@
 import { Router } from "express";
+import * as events from "../controllers/events";
 
 const router = Router();
 
 router.get("/ping", (req, res) => {
 	return res.json({ pong: true });
 });
+
+router.get("/events/:id", events.getEvent);
 
 export default router;
